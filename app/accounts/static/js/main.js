7f13408f381407e8100875109011e3273ef18ca4
heman <heman@Heman.local>
2019-10-14 13:40:43 +1000

    Deleted extra js files  Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- app/accounts/static/js/main.js
+++ app/accounts/static/js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-  var myForm = $('.feedback-form');
+  var myForm = $('.form');
 
   myForm.submit(function(event){
     event.preventDefault();
     $('.info').empty();
     var formData = myForm.serialize();
     var formURL = myForm.attr('action');
+    var path = window.location.pathname;
 
     $.ajax({
       method: 'POST',
@@ -13,16 +14,22 @@ $(document).ready(function(){
       data: formData,
       success: function(data){
         if (data.success){
-          myForm.each(function(){
-              this.reset();
-          });
-          $(".info").append(
-            "<article class=\"message is-success\">" +
-              "<div class=\"message-body\">" +
-                data.message +
-              "</div>" +
-              "</article>"
-          );
+          if(path == '/feedback/') {
+            myForm.each(function(){
+                this.reset();
+            });
+            $(".info").append(
+              "<article class=\"message is-success\">" +
+                "<div class=\"message-body\">" +
+                  data.message +
+                "</div>" +
+                "</article>"
+            );
+          } else if (path == '/signup/') {
+            window.location.replace("http://localhost:8000/");
+          } else {
+            window.location.replace("http://localhost:8000/feedback/");
+          }
         } else {
           $.each(data.errors, function(index, item) {
             $(".info").append(
